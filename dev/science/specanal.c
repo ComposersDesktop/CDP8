b5c5aef56a29d96c4bde01064467622fc2572017
richarddobson <richard@rwdobson.com>
2023-01-02 09:37:51 +0000

    minor fix to top comment block

--- dev/science/specanal.c
+++ dev/science/specanal.c
@@ -19,7 +19,8 @@
  License along with the CDP System; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  02111-1307 USA
- *
+ */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <structures.h>
