3c4eaaf62b5ba5a275f74d31b1f245c22a3767b8
richarddobson <richard@rwdobson.com>
2025-11-27 14:02:23 +0000

    simplified calls to round

--- dev/standalone/mchanpan.c
+++ dev/standalone/mchanpan.c
@@ -45,9 +45,9 @@
 #include <string.h>
 #include <srates.h>
 
-#if defined unix || defined __GNUC__
+ //#ifdef unix 
 #define round(x) lround((x))
-#endif
+//#endif
 #ifndef HUGE
 #define HUGE 3.40282347e+38F
 #endif
