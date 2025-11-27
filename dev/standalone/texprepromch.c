e7ad84647a5913b7c410ebe3edd2a70415f0146c
richarddobson <richard@rwdobson.com>
2025-11-27 14:07:56 +0000

    simplified calls to round

--- dev/standalone/texprepromch.c
+++ dev/standalone/texprepromch.c
@@ -41,9 +41,9 @@
 #include <cdpmain.h>
 #include <standalone.h>
 
-#if defined unix || defined __GNUC__
+//#ifdef unix 
 #define round(x) lround((x))
-#endif
+//#endif
 #ifndef HUGE
 #define HUGE 3.40282347e+38F
 #endif
